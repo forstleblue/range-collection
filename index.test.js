153a7cffca08544ffe490b156e773a729beee3c9
forstleblue <blueskymobile@hotmail.com>
2019-01-16 12:39:53 +0100

    Add more test cases

--- index.test.js
+++ index.test.js
@@ -1,8 +1,23 @@
 const rc = require('range_collection')
 
 rc.add([1, 5]);
+const first = rc.collectionString();
+test('Adding [1, 5]', () => {
+  expect(first).toBe('[1, 5) ')
+});
+
 rc.add([10, 20]);
+const second = rc.collectionString();
+test('Adding [1, 5] [10, 20]', () => {
+  expect(second).toBe('[1, 5) [10, 20) ')
+});
+
 rc.add([20, 20]);
+const third = rc.collectionString();
+test('Adding [1, 5] [10, 20] [20, 20]', () => {
+  expect(third).toBe('[1, 5) [10, 20) ')
+});
+
 rc.add([20, 21]);
 rc.add([2, 4]);
 rc.add([3, 8]);
